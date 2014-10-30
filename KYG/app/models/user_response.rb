80e4cc5ab43c880d3ae35a009672860c75b0108d
adrienkalombo@outlook.com <adrienkalombo@outlook.com>
2014-10-30 18:45:06 -0400

    trying to make this work

--- KYG/app/models/user_response.rb
+++ KYG/app/models/user_response.rb
@@ -11,12 +11,15 @@ class UserResponse < ActiveRecord::Base
   end
 
   def verify_capital
-    if capital != correct_capital
-      errors.add(:capital, "should be #{correct_capital}")
-    end
+    #if capital != correct_capital
+      #errors.add(:capital, "should be #{correct_capital}")
+    #end
   end
 
   def verify_language
+    if capital != correct_language
+      errors.add(:language, "should be #{correct_language}")
+    end
   end
 
 end
